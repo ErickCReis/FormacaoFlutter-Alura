cb4970fa7622a37512fd9850e5c33b853da59e8e
ErickCReis <erickcorreareis@gmail.com>
2022-04-11 23:38:28 -0300

    Fundamentos do Flutter: Aula 02 - Implementando layouts com Widgets

--- Fundamentos de Flutter/README.md
+++ Fundamentos de Flutter/README.md
@@ -8,3 +8,11 @@
 - Criar e executar o primeiro projeto;
 - Abrir o projeto no IntelliJ, editar o código e executá-lo;
 - O que são Widgets e como funciona a estrutura para criar layouts no Flutter.
+
+### Aula 02 - Implementando layouts com Widgets
+
+- A implementar um layout com os componentes do Material Design;
+- Atalhos e dicas para auxiliar a implementação do layout;
+- Refatorar os próprios Widgets;
+- O que é StatelessWidget e StatefulWidget;
+- Organizar e reutilizar código no Flutter.
--- Fundamentos de Flutter/lib/main.dart
+++ Fundamentos de Flutter/lib/main.dart
@@ -2,25 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: <Widget>[
-        const Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tranferências'),
         ),
-        const Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
         ),
-        Column(
-          children: const <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        ),
-      ],
+        body: ListaTranferencias(),
+      ),
     ),
   );
+}
+
+class ListaTranferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTranferencia(Transferencia(100.0, 1000)),
+        ItemTranferencia(Transferencia(200.0, 2000)),
+        ItemTranferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTranferencia extends StatelessWidget {
+  final Transferencia _tranferencia;
+
+  const ItemTranferencia(this._tranferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(_tranferencia.valor.toString()),
+        subtitle: Text(_tranferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
