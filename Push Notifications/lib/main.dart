3045d7b40e4eb6e131bea7caa4e1a02ea28f9945
ErickCReis <erickcorreareis@gmail.com>
2022-06-04 19:11:55 -0300

    Push notifications: Aula 01 - Introdução ao Firebase Cloud Messaging

--- Push Notifications/lib/main.dart
+++ Push Notifications/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:meetups/screens/events_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
